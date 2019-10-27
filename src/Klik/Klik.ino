7068a763094078eb09149f3d7bd6218312537a76
ErinaceusLinnaeus <benji@fstop8.de>
2019-10-27 12:26:41 +0000

    Initial files

---
+++ src/Klik/Klik.ino
@@ -0,0 +1,15 @@
+/* (C)2019 Benji Schmid
+ *  
+ *  Klik is a Metronom that will be part of my recording rack.
+ *  Presumably it will be take up 1 HE and half the rack's length.
+ */
+ 
+ void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
